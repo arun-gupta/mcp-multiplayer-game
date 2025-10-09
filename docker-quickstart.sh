a8d1bc8ff30b63a6b71d1fdca0411fe7d1d5c2a7
dcolazin <dcolazin@gmail.com>
2025-10-09 09:17:50 +0200

    remote telemetry, pass env & ollama

--- docker-quickstart.sh
+++ docker-quickstart.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-docker run -v $(pwd):/gupta gupta-runner /gupta/quickstart.sh $@
+docker run \
+	   -v $(pwd):/gupta -v $(which ollama):/usr/local/bin/ollama -v /usr/share/ollama/:/usr/share/ollama -v $HOME/.ollama:/root/.ollama \
+	   -e OPENAI_API_KEY=$OPENAI_API_KEY -e ANTHROPIC_API_KEY=$ANTHROPIC_API_KEY \
+	   -p 8501:8501 -p 8000:8000 \
+	   -d gupta-runner /gupta/quickstart.sh $@
